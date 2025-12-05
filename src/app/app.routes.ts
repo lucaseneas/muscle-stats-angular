f212bdc67daabff3fa7f66f05863a944e7ed163a
Lucas Eneas <lucaseneasmarco@gmail.com>
2025-12-05 08:36:42 +0000

    feat: implement HomePage component, update application routing, and define global styles.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
+import { HomePage } from './pages/home-page/home-page';
 
 export const routes: Routes = [{
     path: '',
     component: LoginPage,
+  },{
+    path: 'login',
+    component: LoginPage,
+  },
+  {
+    path: 'home',
+    component: HomePage,
   }
 ];
